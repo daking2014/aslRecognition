bc4cb1eababd372c0bd80b75cfb1df1e86db6101
Daniel King <daniel.king2014@gmail.com>
2017-04-07 17:04:20 -0700

    Add top-k accuracy reporting to show_performance

--- show_performance.py
+++ show_performance.py
@@ -5,7 +5,22 @@ from sklearn.externals import joblib
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
-def main(dataFile, clfFile, plot_cm, leave_out):
+def topKAccuracy(yProbabilities, yTrue, k, classes):
+    correct = 0
+    for probs, label in zip(yProbabilities, yTrue):
+        for i in range(k):
+            maxIndex = probs.argmax(axis=0)
+            probs[maxIndex] = 0
+            predictedLabel = classes[maxIndex]
+            if predictedLabel == label:
+                correct += 1
+                break
+
+    return correct/float(len(yTrue))
+
+
+
+def main(dataFile, clfFile, plot_cm, leave_out, k):
     primaryData = np.load(dataFile)
     labels = primaryData['label']
     people = primaryData['person']
@@ -20,8 +35,9 @@ def main(dataFile, clfFile, plot_cm, leave_out):
     yPred = clf.predict(XTrain)
     score = metrics.accuracy_score(yTrain, yPred)
     print "Training Accuracy:", score
+    print "Training set size:", yTrain.shape
     cm = metrics.confusion_matrix(yTrain, yPred)
-    
+
     if plot_cm:
         plt.figure()
     util.plotConfusionMatrix(cm, classes=[chr(ord('a')+c) for c in np.unique(yTrain)],title="Confusion Matrix", plot=plot_cm)
@@ -30,9 +46,12 @@ def main(dataFile, clfFile, plot_cm, leave_out):
 
     yPred = clf.predict(XTest)
     score = metrics.accuracy_score(yTest, yPred)
+    topKScore = topKAccuracy(clf.predict_proba(XTest), yTest, k, clf.classes_)
     print "Test Accuracy:", score
+    print "Top k accuracy for k = " + str(k) + ": " + str(topKScore)
+    print "Testing set size:", yTest.shape
     cm = metrics.confusion_matrix(yTest, yPred)
-    
+
     if plot_cm:
         plt.figure()
     util.plotConfusionMatrix(cm, classes=[chr(ord('a')+c) for c in np.unique(yTest)],title="Confusion Matrix", plot=plot_cm)
@@ -44,6 +63,7 @@ parser.add_argument('dataFile', help='Main data file')
 parser.add_argument('clfFile', help='Classifier file')
 parser.add_argument('--leave-out', type=int, default=3, help='Index of person to leave out')
 parser.add_argument('--disable-cm', dest='plot_cm', action='store_false', help='Do not plot a confusion matrix')
+parser.add_argument('--k', type=int, default=2, help='Value of k for top-k accuracy reporting')
 
 if __name__ == '__main__':
     namespace = parser.parse_args()
